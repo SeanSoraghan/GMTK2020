840c3abf06df8245ba23ff70c59b698951cafe80
SeanSoraghan <sasoraghan@googlemail.com>
2020-07-31 00:25:45 +0100

    attempt at line thickness effect

---
+++ Assets/Scripts/WorldObjects/LineThicknessAnimator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class LineThicknessAnimator : MonoBehaviour
+{
+	public float globalMaxThickness = 0.05f;
+	float centre = 0.0f;
+	LineRenderer lineRenderer;
+
+	private void Awake()
+	{
+		lineRenderer = GetComponent<LineRenderer>();
+		Assert.IsNotNull(lineRenderer);
+	}
+
+	// Start is called before the first frame update
+	void Start()
+    {
+		centre = globalMaxThickness * 0.5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+		lineRenderer.startWidth = centre + Mathf.Sin(Time.time * 3.0f) * centre;
+		lineRenderer.endWidth = centre + Mathf.Cos(Time.time * 5.0f) * centre;
+    }
+}
