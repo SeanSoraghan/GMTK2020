fbf14bebfa1293fd48309e5f88743b4d2f21c966
SeanSoraghan <sasoraghan@googlemail.com>
2020-08-03 10:57:00 +0100

    updated maze cube line renderers

--- Assets/Scripts/WorldObjects/LineCubeController.cs
+++ Assets/Scripts/WorldObjects/LineCubeController.cs
@@ -39,22 +39,23 @@ public class LineCubeController : MonoBehaviour
 			}
 		}
 	}
-	LineRenderer lineRenderer;
+
     // Start is called before the first frame update
     void Awake()
     {
 		DontDestroyOnLoad(gameObject);
 
-		lineRenderer = GetComponent<LineRenderer>();
-		Assert.IsNotNull(lineRenderer);
-		// Since we do this in the editor as well, we don't want to set the position once, then multiply it again every Awake for each PIE.
-		// So we set newExtent as target / current, and multiply by that.
-		float currentExtent = Mathf.Abs(lineRenderer.GetPosition(0).x);
-		float mult = (LevelController.WORLD_CUBE_LIMIT + 0.5f) / currentExtent;
-		for (int p = 0; p < lineRenderer.positionCount; ++p)
+		foreach (LineRenderer lineRenderer in GetComponentsInChildren<LineRenderer>())
 		{
-			Vector3 pos = lineRenderer.GetPosition(p);
-			lineRenderer.SetPosition(p, pos * mult);
+			// Since we do this in the editor as well, we don't want to set the position once, then multiply it again every Awake for each PIE.
+			// So we set newExtent as target / current, and multiply by that.
+			float currentExtent = Mathf.Abs(lineRenderer.GetPosition(0).x);
+			float mult = (LevelController.WORLD_CUBE_LIMIT + 0.5f) / currentExtent;
+			for (int p = 0; p < lineRenderer.positionCount; ++p)
+			{
+				Vector3 pos = lineRenderer.GetPosition(p);
+				lineRenderer.SetPosition(p, pos * mult);
+			}
 		}
     }
 
